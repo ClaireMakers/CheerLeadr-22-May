cabab43434d94b6e5c1fa90337d83362bd3631b5
Claire Castanet <clairecastanet@MASTAFF-Claire-Castanet-Q14CH4R1PW.local>
2024-05-22 12:04:27 +0100

    workshop code plus quick update

--- src/components/CheerButton.jsx
+++ src/components/CheerButton.jsx
@@ -1,15 +1,16 @@
-//props is an object with the key-value pairs you passed down from the parent component.
-// Here is it: {letter: letter, setLetter: f}
-
 const CheerButton = (props) => { 
-     
+
     const handleClick = () => { 
-        props.setLetter(props.letter);
+        props.setLetter(props.letter)
+        //this updates the state in the parent component, triggering a re-render of the App component
     }
 
+    console.log(props) 
+    // {letter: "M", setLetter: setLetter function () => {} }
+ 
     return (
-            <button onClick={ handleClick }>{props.letter}</button>
+        <button onClick={handleClick}>{props.letter}</button>
     )
 }
 
-export default CheerButton
+export default CheerButton;
